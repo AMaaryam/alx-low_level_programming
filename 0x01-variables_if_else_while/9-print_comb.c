32ad81cd3ae5d53ae95a58e9189641808a351a27
AMaaryam <a.maaryam@gmail.com>
2023-03-17 10:54:23 -0700

    9combin

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int x;
+	int i;
 
-    for (x = 0; x < 10; x++)
-    {
-        putchar(x + '0');
-        if (x < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
