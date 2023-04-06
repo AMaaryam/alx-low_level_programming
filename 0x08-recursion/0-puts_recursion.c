dd803d8945b6e0a4d5ab6f0817cde11c1fe338ea
AMaaryam <a.maaryam@gmail.com>
2023-04-05 18:29:26 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,18 +8,14 @@
  */
 
 void _puts_recursion(char *s)
-
 {
-	if (*s=='\0')
-
+	if (*s == '\0')
 	{
-
-		_putchar ('\n');
+		_putchar('\n');
 		return;
-
 	}
 
 	_putchar(*s);
 	s++;
-	_puts_recursions(s);
+	_puts_recursion(s);
 }
