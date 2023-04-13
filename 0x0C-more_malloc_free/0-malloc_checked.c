e9ee64e37c65a73144b92b5010132749dc1d76cd
AMaaryam <a.maaryam@gmail.com>
2023-04-13 08:51:04 -0700

    0task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+/**
+ *  *malloc_checked - Allocates memory using malloc
+ *  *@b: unsigned int argument size of memory
+ *  
+ *  *Description: Allocates memory using malloc and checks for a NULL
+ *  *Return: Pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+    ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);
+    return (ptr);
+}
