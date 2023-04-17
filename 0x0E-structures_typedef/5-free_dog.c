f444d43064c2a5eb1f52391db483d114e26e9da3
AMaaryam <a.maaryam@gmail.com>
2023-04-17 18:17:21 +0000

    typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees typedef dog_t of struct dog
+ * @d: pointer variable to typedef dog_t
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		free(d->name);
+
+	if (d->owner)
+		free(d->owner);
+
+	free(d);}
