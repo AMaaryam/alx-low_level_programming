3ed43fa6a0c82268c84ce4367d8af122d0d77c0a
AMaaryam <a.maaryam@gmail.com>
2023-04-28 01:59:03 +0100

    littel comm

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,29 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
-size_t print_list(const list_t *h)
-{
-    size_t count = 0;
 
-    while (h != NULL)
+    print_list - Prints all the elements of a list_t list.
+    @h: A pointer to the first node of the list_t list.
+    Return: The number of nodes in the list.
+    */
+
+size_t print_list(const list_t *ha)
+{
+size_t nelm; // Variable to keep track of the number of elements
+	     nelm = 0; // Initialize the count to 0
+
+    // Loop through the list until the end is reached
+    while (ha != NULL)
     {
-        printf("[%d] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
+            // Print the current element and increment the count
+            if (ha->str == NULL)
+                    printf("[%d] %s\n", 0, "(nil)");
+            else
+                    printf("[%d] %s\n", ha->len, ha->str);
+
+            ha = ha->next; // Move to the next element
+            nelm++; // Increment the count
     }
 
-    return count;
+    return (nelm); // Return the total couni
 }
