fc2921ed7db06bb59e3d6580f89162fd8443112e
AMaaryam <a.maaryam@gmail.com>
2023-04-28 04:10:48 +0100

    littel comm

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,11 @@
 
 /*Owned By Maryam
  * Function that is automatically executed before main() */
-void __attribute__((constructor)) first() {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+void __attribute__((constructor)) first()
+
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	²
 }
