e48c683c8b668ed222207de539a6a0e7bb029f4c
AMaaryam <a.maaryam@gmail.com>
2023-05-06 00:54:49 +0100

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0; // variable to hold the converted decimal value
+	unsigned int dec_val = 0; /* variable to hold the converted decimal value*/
 
 	if (!b)
-		return (0); // return 0 if input is NULL
+		return (0); /* return 0 if input is NULL*/
 
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-			return (0); // return 0 if input is not a binary number
-		dec_val = 2 * dec_val + (b[i] - '0'); // calculate decimal value from binary
+			return (0); /* return 0 if input is not a binary number*/
+		dec_val = 2 * dec_val + (b[i] - '0'); /* calculate decimal value from binary*/
 	}
 
-	return (dec_val); // return decimal value
+	return (dec_val); /* return decimal value*/
 }
