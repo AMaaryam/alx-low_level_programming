d5a376705c4b46e12874803b14760eed7d4c97fa
AMaaryam <a.maaryam@gmail.com>
2023-05-06 01:16:29 +0100

    first commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+
+    set_bit - sets a bit of a given index to 1
+
+    @numPtr: pointer to the number to be modified
+
+    @bitIndex: index of the bit to be set to 1
+
+    Return: on success 1, on failure -1
+    */
+    int set_bit(unsigned long int *numPtr, unsigned int bitIndex)
+    {
+    /* check if the bit index is within range*/
+    if (bitIndex > 63)
+    {
+    return (-1);
+    } /*set the bit to 1 using bit manipulation*/
+    *numPtr = ((1UL << bitIndex) | *numPtr);
+
+    /* return success*/
+    return (1);
+    }
