0acb7f96b3cd406eb81015e394f10adaaf902b9c
AMaaryam <a.maaryam@gmail.com>
2023-05-08 22:58:51 +0100

    i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,30 @@
-
-
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * read_textfile- Read the tex whicht prints STDOUT.
- * @filename: text reading
- * @letters: The number of characters to read and print.
- *Return: The actual number of bytes read and printed on success,0 if the function fails or if filename is NULL.
+ * read_textfile - Reads the text and prints to STDOUT.
+ * @filename: Name of the file to read.
+ * @max_bytes: Maximum number of bytes to read.
+ *
+ * Return: Number of bytes read and printed. If the function fails or if
+ * the filename is NULL, return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char buf; /* Pointer to allocated buffer.*/
-ssize_t fd; /* File descriptor. */
-ssize_t w; /* Number of characters written to STDOUT. */
-ssize_t t; /* Number of characters read from file. */
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_written;
+	ssize_t bytes_read;
 
-fd = open(filename, O_RDONLY);  /* Open file in read-only mode. */
-if (fd == -1)
-	return (0);  /* Return 0 if opening the file fails. */
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-buf = malloc(sizeof(char) * letters);  /* Allocate memory for buffer. */
-t = read(fd, buf, letters);  /* Read characters from file into buffer. */
-w = write(STDOUT_FILENO, buf, t);  /* Write characters from buffer to STDOUT. */
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-free(buf);  /* Free the memory allocated for buffer. */
-close(fd);  /* Close the file. */
-return (w);  /* Return the number of characters written to STDOUT. */
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
